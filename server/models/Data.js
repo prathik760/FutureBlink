69eb07921b040f0554e0c4eb430c93fb3434b511
Prathik-aow <prathik@aow.co.in>
2026-03-25 15:08:45 +0530

    fix: restore unintentionally removed backend files

---
+++ server/models/Data.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const DataSchema = new mongoose.Schema(
+  {
+    prompt: { type: String, required: true },
+    response: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Data", DataSchema);
